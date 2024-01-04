d34d6bc5629a64cd960a66a12631cdfc8b7c1c5c
muchira007 <muchirasteve2@gmail.com>
2024-01-04 05:56:41 -0800

    Import a simple function from a simple file- error handling

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+if __name__ = "main":
+    from add_0 import add
+    a = 1
+    b = 2
 
-a = 1
-b = 2
-
-from add_0 import add
-
-result = add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, add(a, b)))
 
