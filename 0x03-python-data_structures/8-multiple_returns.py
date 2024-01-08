8b54eeec20f75f402a45d82949e48c3ff0122bf5
muchira007 <muchirasteve2@gmail.com>
2024-01-08 05:22:31 -0800

    more about using lists

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0] if length> 0 else "None"
+    tup = length, first
+    return(tup)
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length < 0:
+        return (None)
+    x = my_list[0]
+    for i in my_list:
+        if x < i:
+            x = i
+    return (i)
