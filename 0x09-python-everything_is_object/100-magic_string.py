8d2b47319638e4d85c78d62f9ba8dd5321ac225f
muchira007 <muchirasteve2@gmail.com>
2024-01-30 11:28:42 -0800

    finished

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+100-magic_string.py
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
